3c20ca9b35457c8be7e9a9312fbfb428357456da
syn <tlsdpsk1992@naver.c0m>
2025-04-16 09:31:10 +0900

    [feat] 엔티티 생성

---
+++ src/main/java/com/fouribnb/coupon/domain/entity/Coupon.java
@@ -0,0 +1,58 @@
+package com.fouribnb.coupon.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "p_coupon")
+@Getter
+@NoArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, name = "coupon_id")
+    private UUID id;
+
+    @Column(nullable = false, name = "user_id")
+    private Long userId;
+
+    @Column( name = "order_id")
+    private UUID orderId;
+
+    @Column(nullable = false, name = "coupon_name")
+    private String couponeName;
+
+    @Column(nullable = false, name = "coupon_status")
+    @Enumerated(EnumType.STRING)
+    private CouponStatus couponStatus;
+
+    @Column(nullable = false, name = "discount_value")
+    private int discountValue;
+
+    @Column(nullable = false, name = "is_used")
+    private boolean isUsed;
+
+    @Builder
+    public Coupon(Long userId, UUID orderId, String couponeName, CouponStatus couponStatus,
+            int discountValue, boolean isUsed) {
+        this.userId = userId;
+        this.orderId = orderId;
+        this.couponeName = couponeName;
+        this.couponStatus = couponStatus;
+        this.discountValue = discountValue;
+        this.isUsed = isUsed;
+    }
+}
+
