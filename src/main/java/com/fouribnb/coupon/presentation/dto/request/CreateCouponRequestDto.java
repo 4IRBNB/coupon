fb37251954511ae11a20f755bccfb47348214024
syn <tlsdpsk1992@naver.c0m>
2025-04-16 09:31:45 +0900

    [feat] dto, mapper 생성

---
+++ src/main/java/com/fouribnb/coupon/presentation/dto/request/CreateCouponRequestDto.java
@@ -0,0 +1,17 @@
+package com.fouribnb.coupon.presentation.dto.request;
+
+import com.fouribnb.coupon.domain.entity.CouponStatus;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CreateCouponRequestDto {
+
+    private Long userId;
+    private UUID orderId;
+    private String couponName;
+    private int discountValue;
+
+}
---
+++ src/main/java/com/fouribnb/coupon/presentation/mapper/CouponMapper.java
@@ -0,0 +1,32 @@
+package com.fouribnb.coupon.presentation.mapper;
+
+import com.fouribnb.coupon.domain.entity.Coupon;
+import com.fouribnb.coupon.domain.entity.CouponStatus;
+import com.fouribnb.coupon.presentation.dto.request.CreateCouponRequestDto;
+import com.fouribnb.coupon.presentation.dto.response.CreateCouponResponseDto;
+
+public class CouponMapper {
+
+    public static Coupon createDtoToEntity(CreateCouponRequestDto dto) {
+        return Coupon.builder()
+                .userId(dto.getUserId())
+                .orderId(dto.getOrderId())
+                .couponeName(dto.getCouponName())
+                .couponStatus(CouponStatus.ACTIVE)
+                .discountValue(dto.getDiscountValue())
+                .isUsed(false)
+                .build();
+    }
+
+    public static CreateCouponResponseDto createDtoToResponse(Coupon coupon) {
+        return CreateCouponResponseDto.builder()
+                .userId(coupon.getUserId())
+                .orderId(coupon.getOrderId())
+                .couponName(coupon.getCouponeName())
+                .couponStatus(coupon.getCouponStatus())
+                .discountValue(coupon.getDiscountValue())
+                .isUsed(coupon.isUsed())
+                .build();
+    }
+
+}
