510f58965e326c8c040cde8008821dbab839cc51
syn <tlsdpsk1992@naver.c0m>
2025-04-16 10:56:49 +0900

    [feat] Mapper, dto 작성

---
+++ src/main/java/com/fouribnb/coupon/presentation/dto/request/UpdateCouponRequestDto.java
@@ -0,0 +1,14 @@
+package com.fouribnb.coupon.presentation.dto.request;
+
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UpdateCouponRequestDto {
+
+    private String couponName;
+    private int discountValue;
+
+}
