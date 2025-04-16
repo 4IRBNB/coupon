abdbc75931db7d01c2f61138f168ef6aebdb3e72
syn <tlsdpsk1992@naver.c0m>
2025-04-16 10:35:10 +0900

    [feat] 쿠폰조회 서비스 작성

--- src/main/java/com/fouribnb/coupon/application/service/CouponService.java
+++ src/main/java/com/fouribnb/coupon/application/service/CouponService.java
@@ -2,10 +2,14 @@ package com.fouribnb.coupon.application.service;
 
 import com.fouribnb.coupon.presentation.dto.request.CreateCouponRequestDto;
 import com.fouribnb.coupon.presentation.dto.response.CreateCouponResponseDto;
+import com.fouribnb.coupon.presentation.dto.response.GetCouponResponseDto;
+import java.util.UUID;
 
 
 public interface CouponService {
 
     CreateCouponResponseDto createCoupon(CreateCouponRequestDto request);
+
+    GetCouponResponseDto getCoupon(UUID id);
 
 }
