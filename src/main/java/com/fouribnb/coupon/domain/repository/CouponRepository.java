4ad8fe5be67a5ae601717ab580452fb77a576aa2
syn <tlsdpsk1992@naver.c0m>
2025-04-16 10:35:02 +0900

    [feat] 쿠폰조회 리포지토리 작성

--- src/main/java/com/fouribnb/coupon/domain/repository/CouponRepository.java
+++ src/main/java/com/fouribnb/coupon/domain/repository/CouponRepository.java
@@ -1,8 +1,12 @@
 package com.fouribnb.coupon.domain.repository;
 
 import com.fouribnb.coupon.domain.entity.Coupon;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CouponRepository {
     Coupon save(Coupon coupon);
+
+    Optional<Coupon> findById(UUID id);
 
 }
