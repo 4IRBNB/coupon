afbe07539154bf14ad7ef3571cf22d3608a77907
syn <tlsdpsk1992@naver.c0m>
2025-04-27 15:34:55 +0900

    [feat] 유레카 설정

--- src/main/java/com/fouribnb/coupon/CouponApplication.java
+++ src/main/java/com/fouribnb/coupon/CouponApplication.java
@@ -2,7 +2,9 @@ package com.fouribnb.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient    // Eureka 클라이언트 활성화
 @SpringBootApplication
 public class CouponApplication {
 
